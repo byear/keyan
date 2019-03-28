1b911ac15389b154968dae0d2871e56f60cb78a7
xww2652008969 <2652008969@qq.com>
2019-03-28 18:01:17 +0800

    重写构建文件

--- keyan/bulid.sh
+++ keyan/bulid.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 #linux 部署系统
-sudo apt install python  > /dev/null 2>&1
-sudo apt install python3  > /dev/null 2>&1
-pip install flask  > /dev/null 2>&1
-pip install flask  SQLAlchemy  > /dev/null 2>&1
-pip install flask  flask_SQLAlchemy  > /dev/null 2>&1
-pip install flask  lxml  > /dev/null 2>&1
-pip install flask  html  > /dev/null 2>&1
-echo "应用马上执行"
-python app.py 
+py3=/usr/bin/python3
+if [ -e $py3 ]
+then
+echo "python3存在" >.log 2>&1
+else
+sudo apt install python3 >.log 2>&1
+sudo apt install python3-pip >.log 2>&1
+echo "安装安装python3"
+fi
